900ce198df117db006f964a4aa1697a47e133252
jooho <ravenerkr@gmail.com>
2019-09-26 17:49:33 +0900

    머티리얼 색 바꿔주는 부분을 뺌으로서 배칭성공. 성능 대폭 상승.

--- Assets/SimplePathFindAgent3D.cs
+++ Assets/SimplePathFindAgent3D.cs
@@ -18,16 +18,16 @@ public class SimplePathFindAgent3D : MonoBehaviour
 
     private async void Start()
     {
-        while (true)
+        /*while (true)
         {
             await Wait();
             RequestPathFind();
-        }
+        }*/
     }
 
     private void RequestPathFind()
     {
-        MeshRenderer.material.color = Color.red;
+        //MeshRenderer.material.color = Color.red;
         var start = this.transform.position;
         var end = openSpaces[Random.Range(0, openSpaces.Count)];
         Seeker.StartPath(start, end, Callback);
@@ -35,32 +35,29 @@ public class SimplePathFindAgent3D : MonoBehaviour
 
     private void Callback(Path p)
     {
-        MeshRenderer.material.color = Color.green;
+        // MeshRenderer.material.color = Color.green;
         //Debug.Log($"{this.gameObject.name} found path in {p.duration} ms!");
     }
 
     private async Task Wait()
     {
-        MeshRenderer.material.color = Color.blue;
+        //MeshRenderer.material.color = Color.blue;
         await Task.Delay(TimeSpan.FromSeconds(Random.Range(WaitTimeMin, WaitTimeMax)));
     }
 
-    //실제로 이동하는 부분은 너무 느리다. 업데이트 연산량이 과도함.
-    //1000마리 뿌리면 그냥 이동하는 것 만으로도 40프레임까지 떨어짐.
-    //우선 길찾기 함수 호출의 부하만 측정하고 싶으므로, 업데이트를 하지 않고 코루틴만 쓰도록 수정한다.
-    /*private void Update()
+    private void Update()
     {
-        /*if (CloseEnough(_target))
+        if (CloseEnough(_target))
         {
             ResetTarget();
-            MeshRenderer.material.color = Color.green;
+            //MeshRenderer.material.color = Color.green;
         }
         else
         {
             transform.Translate(Vector3.Normalize(_target - transform.position) * Speed);
-            MeshRenderer.material.color = Color.blue;
-        }#1#
-    }*/
+            //MeshRenderer.material.color = Color.blue;
+        }
+    }
 
     private bool CloseEnough(Vector3 target)
     {
@@ -69,6 +66,6 @@ public class SimplePathFindAgent3D : MonoBehaviour
 
     private void ResetTarget()
     {
-        _target = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+        _target = new Vector3(Random.Range(-400, 400), 0, Random.Range(-400, 400));
     }
 }
