4923e51485964cf91514c059a5a5816e48ed9a46
jooho <ravenerkr@gmail.com>
2019-09-28 13:55:04 +0900

    다이나믹 오브젝트를 피해가는 AI로 변경됨.
    구현방법 : 다이나믹 오브젝트에 리지드바디-키네마틱을 걸고, 에이전트에 OnTriggerEnter를 걸어준 후, 에이전트의 콜라이더에 isTrigger를 달아줌. (다이나믹 오브젝트에 IsTrigger를 달아줄 경우, A*가 스캔할 때 무시하는 문제가 발생하기 때문에 이렇게 됨) 길막이 발생했을 때만 지능적 AI로 변경되는 식이므로, 부하를 억제할 수 있다.

--- Assets/Custom/CustomAiDestinationSetter.cs
+++ Assets/Custom/CustomAiDestinationSetter.cs
@@ -7,6 +7,10 @@ namespace Pathfinding
     {
         private GameController gameController;
         public static int Amount;
+        public GameObject HighIQIndicator;
+
+        //에이전트가 어딘가에 막히거나 등 이런 저런 이유로 현재의 패스를 따라가는데 문제가 있음을 의미.
+        private bool _isPathHasTrouble;
 
         /// <summary>The object that the AI should move to</summary>
         public Vector3 destination;
@@ -30,22 +34,39 @@ namespace Pathfinding
             Amount--;
         }
 
+        private void OnTriggerEnter(Collider other)
+        {
+            //Debug.Log($"triggerEnter! {other.gameObject.name}");
+            _isPathHasTrouble = true;
+        }
+
         private void Start()
         {
             SetDestination();
         }
 
+        //목적지에 도달하면 안전상태가 된다. 안전상태는 ai가 실시간 검색을 하지 않으므로 부하가 적다.
+        //어딘가에 부딪히거나 하면 비 안전상태가 되는데, 이때 ai는 목적지에 도달할 때 까지 실시간 검색을 켠다. (지능이 올라감)
+        //이렇게 하여 부하를 줄이면서 지능적인 경로를 따라갈 수 있다.
         private void Update()
         {
             if (ai.reachedEndOfPath)
             {
                 ai.canSearch = true;
+                _isPathHasTrouble = false;
                 SetDestination();
+            }
+            else if (_isPathHasTrouble)
+            {
+                ai.canSearch = true;
             }
             else
             {
                 ai.canSearch = false;
             }
+
+            //고급 인공지능 표시 기능
+            HighIQIndicator.SetActive(ai.canSearch);
         }
 
         private void SetDestination()
