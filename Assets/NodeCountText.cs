ec639a935d2dc510d275b8d77462ef5a156cffa6
jooho <ravenerkr@gmail.com>
2019-09-29 15:50:46 +0900

    리캐스트 그래프를 추가. 노드 개수 체크.

---
+++ Assets/NodeCountText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class NodeCountText : MonoBehaviour
+{
+    private TextMeshProUGUI TMP;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        TMP = GetComponent<TextMeshProUGUI>();
+        StartCoroutine(Count());
+    }
+
+    private IEnumerator Count()
+    {
+        while (true)
+        {
+            var aStarPath = AstarPath.active;
+            var graphs = aStarPath.graphs;
+            var text = "";
+            foreach (var graph in graphs)
+            {
+                text += graph.name;
+                text += " : ";
+                text += graph.CountNodes();
+                text += "  nodes ";
+                text += "\r\n";
+            }
+            TMP.text = text;
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
